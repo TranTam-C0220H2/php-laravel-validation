97c50c2805443aae7497c1003e93670ec86d463c
TranTam-C0220H2 <tam.tvt.ptit@gmail.com>
2020-05-29 11:39:56 +0700

    up

---
+++ app/Http/Requests/PostValication.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostValication extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'number' => 'required|numeric'
+        ];
+    }
+
+    function messages()
+    {
+        return [
+            'number.required' => 'Không được để trống',
+            'number.numeric' => 'Phải là số',
+        ];
+    }
+}
